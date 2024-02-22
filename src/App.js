217d89e729d585603f0293803c8a35a27f16ed11
sufiyanashraf7 <sufiyanashraf7@gmail.com>
2024-02-22 18:46:04 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -7,10 +7,41 @@ import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <AppProvider>
+            <div className="container">
+
+                <h1 className='mt-3'>Shopping App</h1>
+
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <CartValue />
+                    </div>
+                    <div className='col-sm'>
+                        <Location />
+                    </div>
+                </div>
+
+                <h3 className='mt-3'>Shopping Cart</h3>
+
+
+                <div className='row'>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+
+
+                <h3 className='mt-3'>Add Item</h3>
+
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <ItemSelected />
+                    </div>
+                </div>
+            </div>
+        </AppProvider>
+    );
 }
 
 export default App;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,28 @@
 import React, { useContext } from 'react';
+import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
+    const {expenses} = useContext(AppContext);
+
+    return(
+        <table className='table'>
+            <thead className='thead-light'>
+                <tr>
+                    <th scope='col'>Items</th>
+                    <th scope='col'>Quantity</th>
+                    <th scope='col'>Unit Price</th>
+                    <th scope='col'>Items Price</th>
+                    <th scope='col'>Remove</th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map((expenses) => (
+                    <ExpenseItem id={expenses.id} key={expenses.id} name={expenses.name} quantity={expenses.quantity} unitprice={expenses.unitprice}></ExpenseItem>
+                ))}
+            </tbody>
+        </table>
+    );
 };
 
 export default ExpenseList;
